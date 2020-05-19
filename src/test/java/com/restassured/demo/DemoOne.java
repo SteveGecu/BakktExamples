4ee5d770c49bceb76d630c1acbf3aa0b598d48df
serdarge <stevegecu@gmail.com>
2020-05-19 09:28:42 -0400

    some restassured examples

--- src/test/java/com/restassured/demo/DemoOne.java
+++ src/test/java/com/restassured/demo/DemoOne.java
@@ -2,7 +2,9 @@ package com.restassured.demo;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import java.util.Map;
 
@@ -11,24 +13,24 @@ import static io.restassured.RestAssured.given;
 public class DemoOne {
 
     @Test
-    public void Test2(){
+    @DisplayName("Validating status code and extracting payload")
+    public void Test1(){
         String URL = "https://gecho.gigalixirapp.com/api/SteveGecu/FirstSchema/query";
         String query="{\"query\":\"{\\n    getAuthors {\\n        id\\n        name\\n        company\\n    }\\n}\",\"variables\":{}}";
-
 
         given()
                 .contentType(ContentType.JSON)
                 .body(query)
-                .when()
+        .when()
                 .post(URL)
                 .prettyPeek()
-                .then()
+        .then()
                 .statusCode(200);
-
     }
 
-    @Test
-    public void Test3(){
+    @Test()
+    @DisplayName("Extracting id, name and company name from first query")
+    public void Test2(){
         String URL = "https://gecho.gigalixirapp.com/api/SteveGecu/FirstSchema/query";
         String query="{\"query\":\"{\\n    getAuthors {\\n        id\\n        name\\n        company\\n    }\\n}\",\"variables\":{}}";
 
@@ -36,13 +38,23 @@ public class DemoOne {
                 given()
                         .contentType(ContentType.JSON)
                         .body(query)
-                        .when()
+                .when()
                         .post(URL);
 
+        Assert.assertEquals(200,response.statusCode());
+
+        //extracting as Map
         Map<Object,Object> firstSchema = response.as(Map.class);
+
         //System.out.println("firstSchema = " + firstSchema);
-        //System.out.println(firstSchema.get("data"));
-        System.out.println(response.jsonPath().getString("data.getAuthors[0].id"));
+
+        String companyName = response.jsonPath().getString("data.getAuthors[0].company");
+        String name = response.jsonPath().getString("data.getAuthors[0].name");
+        String idNumber = response.jsonPath().getString("data.getAuthors[0].id");
+        System.out.println("idNumber is= " + idNumber);
+        System.out.println("name is= " + name);
+        System.out.println("companyName is= " + companyName);
+
 
     }
 }
